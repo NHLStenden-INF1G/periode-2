213d88a22176a9bad716e78ad3e233dc37f7335e
RemcoHas <rhassing@outlook.com>
2021-01-07 14:59:42 +0100

    Register/login fixes

--- config/classes/core.php
+++ config/classes/core.php
@@ -1,33 +1,30 @@
 <?php
 class Core
 {
-	private $_post;
 
 	function __construct()
 	{
-		global $DB, $filter;
+		global $DB;
 		
-	
-		//echo (Config::$error_filter == true) 	? '<pre>'.$filter->debug.'</pre>' 	: false;
-
 		define('URL', 'http://'.$_SERVER['SERVER_NAME']);
 		define('WebStyle', '/'.styleFolder.'assets');
 
-		/*
-		ini_set('display_errors', 'On');
-
 		//PHP SETTINGS
+
+		//ini_set('display_errors', 'On');
+
 		ini_set('max_execution_time', '300'); //Maximale executietijd
 		ini_set('max_input_time', '60'); //Maximale input tijd
 		ini_set('memory_limit', '5120M'); //Max geheugen
-		*/
+		
 		session_start();
 
 	}
 
 	function __destruct()
 	{
-
+		global $filter;
+		echo (Config::$error_filter == true) ? '<pre>'.$filter->debug.'</pre>' 	: false;
 	}
 
 	function Redirect($link)
